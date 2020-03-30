d38d9081054cb7b8edc148c9cd6693645d4b3f5f
ZORDE <49993749+ZORDE@users.noreply.github.com>
2020-03-30 22:09:41 +0200

    Previous version isn't working, this one is perfectly working

--- Assets/Scripts/VegGeneration.cs
+++ Assets/Scripts/VegGeneration.cs
@@ -7,23 +7,36 @@ public class VegGeneration : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer[] vegsArr;
     private TilemapCollider2D groundCollider;
-    private TilemapRenderer ground;
-    private float groundColliderTop;
+    private TilemapRenderer groundSprite;
+    private Tilemap ground;
+    private float groundCells;
+    private float vegYPos;
     private int randomIndex;
     private float borderX;
 
     private void Awake()
     {
-        ground = GetComponent<TilemapRenderer>();
+        ground = GetComponent<Tilemap>();
+        groundSprite = GetComponent<TilemapRenderer>();
         groundCollider = GetComponent<TilemapCollider2D>();
 
-        for (int i = 0, limit = 5; i < limit; ++i)
+        
+
+        for (int i = 0, limit = 15; i < limit; ++i)
         {
             randomIndex = Random.Range(0, vegsArr.Length);
-            borderX = ground.transform.position.x + ground.bounds.extents.x;
-            groundColliderTop = groundCollider.transform.position.y + groundCollider.bounds.extents.y;
-            Instantiate(vegsArr[randomIndex]);
-            vegsArr[randomIndex].transform.position = new Vector3(Random.Range(-borderX, borderX), groundColliderTop + vegsArr[randomIndex].bounds.extents.y);
+            borderX = groundSprite.transform.localPosition.x + groundSprite.bounds.extents.x;
+
+            //transform.y - half.y + x * cellsize
+            vegYPos = ground.transform.position.y - ground.LocalToWorld(ground.localBounds.extents).y + ground.cellSize.y * (ground.size.y / 3) + vegsArr[randomIndex].bounds.extents.y;
+
+            Instantiate(vegsArr[randomIndex], new Vector3(Random.Range(-borderX, borderX), vegYPos ), vegsArr[randomIndex].transform.rotation);
+            Debug.Log("vegsExtentsY " + vegsArr[randomIndex].bounds.extents.y);
+            
         }
+
+        Debug.Log("vegYPos " + vegYPos);
+        Debug.Log("groundCellsize " + ground.size.y);
+        
     }
 }

87ec7a3bc9b034dff9a32a4c251d34256559037d
ZORDE <49993749+ZORDE@users.noreply.github.com>
2020-03-31 20:15:43 +0200

    Handling all types of vegetation and their occurence in the level

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -7,18 +7,30 @@ public class Parallax : MonoBehaviour
 {
     [SerializeField] private float parallaxHorizontal;
     [SerializeField] private GameObject cam;
-    private float startPos;
+    private float startPos, spriteLenght;
 
     private void Start()
     {
         startPos = transform.position.x;
+        spriteLenght = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     private void FixedUpdate()
     {
+        //float temp = (cam.transform.position.x * (1 - parallaxHorizontal));
         float dist = cam.transform.position.x * parallaxHorizontal;
-
+        
         transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        //if (temp > startPos + spriteLenght)
+        //{
+        //    startPos += spriteLenght;
+        //}
+        //
+        //else if (temp < startPos - spriteLenght)
+        //{
+        //    startPos -= spriteLenght;
+        //}
 
     }
 
--- Assets/Scripts/VegGeneration.cs
+++ Assets/Scripts/VegGeneration.cs
@@ -5,38 +5,61 @@ using UnityEngine.Tilemaps;
 
 public class VegGeneration : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer[] vegsArr;
-    private TilemapCollider2D groundCollider;
+    [SerializeField] private SpriteRenderer[] lil_plantsArr;
+    [SerializeField] private int lil_plantsNb;
+    [SerializeField] private SpriteRenderer[] big_plantsArr;
+    [SerializeField] private int big_plantsNb;
+    [SerializeField] private SpriteRenderer[] treesArr;
+    [SerializeField] private int treesNb;
+    [SerializeField] private SpriteRenderer grass;
+    
+    
+    private int grassNb;
+    private float prevGrassPosX;
     private TilemapRenderer groundSprite;
-    private Tilemap ground;
-    private float groundCells;
+    private Tilemap ground;  
     private float vegYPos;
     private int randomIndex;
-    private float borderX;
+    private float limitX;
 
     private void Awake()
     {
         ground = GetComponent<Tilemap>();
         groundSprite = GetComponent<TilemapRenderer>();
-        groundCollider = GetComponent<TilemapCollider2D>();
 
-        
+        limitX = groundSprite.transform.localPosition.x + groundSprite.bounds.extents.x;
+        grassNb = (int)(groundSprite.bounds.size.x / grass.bounds.extents.x);
+        prevGrassPosX = -limitX + grass.bounds.extents.x;
 
-        for (int i = 0, limit = 15; i < limit; ++i)
+        //position transform.y - half.y + x * cellsize
+        vegYPos = ground.transform.position.y - ground.LocalToWorld(ground.localBounds.extents).y + ground.cellSize.y * (ground.size.y / 3);
+
+        //lil plants
+        for (int i = 0; i < lil_plantsNb; ++i)
         {
-            randomIndex = Random.Range(0, vegsArr.Length);
-            borderX = groundSprite.transform.localPosition.x + groundSprite.bounds.extents.x;
-
-            //transform.y - half.y + x * cellsize
-            vegYPos = ground.transform.position.y - ground.LocalToWorld(ground.localBounds.extents).y + ground.cellSize.y * (ground.size.y / 3) + vegsArr[randomIndex].bounds.extents.y;
-
-            Instantiate(vegsArr[randomIndex], new Vector3(Random.Range(-borderX, borderX), vegYPos ), vegsArr[randomIndex].transform.rotation);
-            Debug.Log("vegsExtentsY " + vegsArr[randomIndex].bounds.extents.y);
-            
+            randomIndex = Random.Range(0, lil_plantsArr.Length);
+            Instantiate(lil_plantsArr[randomIndex], new Vector3(Random.Range(-limitX, limitX), vegYPos + lil_plantsArr[randomIndex].bounds.extents.y), lil_plantsArr[randomIndex].transform.rotation);
         }
-
-        Debug.Log("vegYPos " + vegYPos);
-        Debug.Log("groundCellsize " + ground.size.y);
         
+        //big_plants
+        for (int i = 0; i < big_plantsNb; ++i)
+        {
+            randomIndex = Random.Range(0, big_plantsArr.Length);
+            Instantiate(big_plantsArr[randomIndex], new Vector3(Random.Range(-limitX, limitX), vegYPos + big_plantsArr[randomIndex].bounds.extents.y), big_plantsArr[randomIndex].transform.rotation);
+        }
+        
+        //trees
+        for (int i = 0; i < treesNb; ++i)
+        {
+            randomIndex = Random.Range(0, treesArr.Length);
+            Instantiate(treesArr[randomIndex], new Vector3(Random.Range(-limitX, limitX), vegYPos + treesArr[randomIndex].bounds.extents.y), treesArr[randomIndex].transform.rotation);
+        }
+        
+        //grass
+        for (int i = 0; i < grassNb; ++i)
+        {
+            Instantiate(grass, new Vector3(prevGrassPosX, vegYPos + grass.bounds.extents.y), grass.transform.rotation);
+            prevGrassPosX += grass.bounds.extents.x;
+        }      
     }
 }

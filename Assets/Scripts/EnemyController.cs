757c99362171a7f0dde0b93ab759c0b982dbce0a
ZORDE <49993749+ZORDE@users.noreply.github.com>
2020-04-04 21:21:08 +0200

    Implemented move function

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController
+{
+    public void move(Rigidbody2D rb, float dir)
+    {
+        rb.velocity = new Vector2(dir, rb.transform.position.y);
+
+        if (dir < 0)
+        {
+            rb.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        else
+        {
+            rb.GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+}

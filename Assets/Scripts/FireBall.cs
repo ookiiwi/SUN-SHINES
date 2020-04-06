a185dede9727b3fa669d7842735dc3997631ae35
ZORDE <49993749+ZORDE@users.noreply.github.com>
2020-04-06 22:33:20 +0200

    Manage the lifetime of the fireBall and transfer control to Hurt state of the target

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    private EnemyAI enemyAI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * moveSpeed;
+        enemyAI = FindObjectOfType<EnemyAI>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Destroy " + collision.name);
+
+        if (collision.tag != "Player")
+        {
+            enemyAI.state = EnemyAI.State.Hurt;
+            Destroy(gameObject);
+        }           
+    }
+
+}

5b13811206a774cfb504493910ba159ca5f68aac
ZORDE <49993749+ZORDE@users.noreply.github.com>
2020-04-06 22:30:06 +0200

    Manage attacks instanciation

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Transform firePoint;
+    public GameObject fireBall;
+    
+
+    private void Awake()
+    {
+        firePoint = GameObject.FindGameObjectWithTag("Hit Point").GetComponent<Transform>();
+    }
+
+    public void Attack()
+    {
+        Instantiate(fireBall, new Vector2(firePoint.position.x, firePoint.position.y), firePoint.rotation);
+    }
+}

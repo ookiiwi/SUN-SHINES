06dc0d372edb7d85bb65e7043a05e3ce11779357
ZORDE <49993749+ZORDE@users.noreply.github.com>
2020-04-06 22:12:25 +0200

    Implemented FlipX() that replace .flipX to sync shoot dir

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,46 +6,72 @@ using UnityEngine.Events;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float MOVE_SPEED = 4f;
-    [SerializeField] private int JUMP_VELOCITY = 9;
+    [SerializeField] private float JUMP_VELOCITY = 5f;
     [SerializeField] private LayerMask GroundLayerMask;
     private Rigidbody2D rb;
     public Animator animator;
     private SpriteRenderer spriteRenderer;
     private BoxCollider2D boxCollider2D;
+    private Transform firePoint;
+    private bool facingRight = true;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         boxCollider2D = transform.GetComponent<BoxCollider2D>();
-        
+        firePoint = GameObject.FindGameObjectWithTag("Hit Point").GetComponent<Transform>();
+
     }
 
     public void Run(float moveX)
     {
-        rb.velocity = new Vector2(moveX * MOVE_SPEED, rb.velocity.y);
-        spriteRenderer.flipX = moveX > 0 ? false : true;
-        animator.SetFloat("Speed", Mathf.Abs(moveX));
+        if (OnLand())
+        {
+            rb.velocity = new Vector2(moveX * MOVE_SPEED, rb.velocity.y);
+            //spriteRenderer.flipX = moveX > 0 ? false : true;
+
+            if (moveX > 0 && !facingRight)
+            {
+                FlipX();
+            }
+
+            else if (moveX < 0 && facingRight)
+            {
+                FlipX();
+            }
+            //animator.SetBool("IsRunning", true);
+            
+        }
     }
 
     public void Jump()
     {
         if (OnLand())
         {
-            rb.velocity = Vector2.up * JUMP_VELOCITY;
-            animator.SetBool("IsJumping", true);
+            //animator.SetBool("IsJumping", true);
+            rb.velocity = new Vector2(rb.velocity.normalized.x, Vector2.up.y) * JUMP_VELOCITY;
+            
+            Debug.Log("Jumping");
         }
     }
 
-    private bool OnLand()
+    public bool OnLand()
     {
         if (boxCollider2D.IsTouchingLayers(GroundLayerMask))
         {
-            //Debug.Log("OnLand");
+            Debug.Log("OnLand");
             return true;
         }
 
-        //Debug.Log("!OnLand");
+        Debug.Log("!OnLand");
         return false;
+    }
+
+    private void FlipX()
+    {
+        facingRight = !facingRight;
+
+        transform.Rotate(0f, 180f, 0f);
     }
 }

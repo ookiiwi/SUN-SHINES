37b008dace29d0a1f302eccb2533fd564eb0db15
ZORDE <49993749+ZORDE@users.noreply.github.com>
2020-04-16 19:17:08 +0200

    Added push action

---
+++ Assets/Scripts/Player/P_Push.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_Push : StateMachineBehaviour
+{
+    private PlayerMovement playerMovement;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerMovement = animator.GetComponent<PlayerMovement>();
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Input.GetAxis("Horizontal") == 0)
+            animator.SetBool("IsPushing", false);
+
+        playerMovement.Run(Input.GetAxis("Horizontal"));
+    }
+}
--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -7,36 +7,56 @@ public class PlayerManager : MonoBehaviour
     public bool isAI;
     public Animator animator;
 
+    private PlayerMovement playerMovement;
+
     public float initDelayAttacks;
     private float delayAttacks;
 
     private void Start()
     {
+        playerMovement = GetComponent<PlayerMovement>();
+
         delayAttacks = 0;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) && delayAttacks <= 0 && !isAI)
+        if (!isAI)
         {
-            animator.SetTrigger("Attack");
+            gameObject.GetComponent<PlayerAI>().enabled = false;
 
-            delayAttacks = initDelayAttacks;
-        }
+            if (Input.GetKeyDown(KeyCode.A) && delayAttacks <= 0)
+            {
+                animator.SetTrigger("Attack");
 
-        else if (delayAttacks > 0)
-        {
-            delayAttacks -= Time.deltaTime;
-        }
+                delayAttacks = initDelayAttacks;
+            }
 
-        if (isAI)
-        {
-            gameObject.GetComponent<PlayerAI>().enabled = true;
+            else if (delayAttacks > 0)
+            {
+                delayAttacks -= Time.deltaTime;
+            }
         }
         
         else
         {
-            gameObject.GetComponent<PlayerAI>().enabled = false;
+            gameObject.GetComponent<PlayerAI>().enabled = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isAI)
+        {
+            if (collision.otherCollider.transform.position.x < collision.collider.transform.position.x && playerMovement.facingRight)
+            {
+                animator.SetBool("IsPushing", true);
+            }
+
+            else if (collision.otherCollider.transform.position.x > collision.collider.transform.position.x && !playerMovement.facingRight)
+            {
+                animator.SetBool("IsPushing", true);
+            }
         }
     }
 }
